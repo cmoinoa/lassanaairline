9f993c1f8f0be3985dd7d6b3f19a3ab4b4f16fe2
hasbulla10 <133578417+hasbulla10@users.noreply.github.com>
2025-03-28 14:18:25 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription - Lassana Airline</title>
-    <link rel="stylesheet" href="css/inscription.css">
+    <link rel="stylesheet" href="css/lassana airlines.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
 </head>
 <body>
